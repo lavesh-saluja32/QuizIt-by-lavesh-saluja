ee4864928f82fe0560716420bc5f8a757d00172a
Lavesh Saluja <lavesh.saluja@bigbinary.com>
2025-04-13 16:43:39 +0530

    Rails review 1 (#112)
    * completed-rails-review-1
    
    * test corrected
    
    ---------
    
    Co-authored-by: admin <admin@admins-MacBook-Pro.local>

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::CategoriesController < ApplicationController
   after_action :verify_authorized, except: :index
   skip_before_action :authenticate_user_using_x_auth_token, only: :index
   def index
-    @categories = Api::V1::CategoriesFilterService.new(params, Category.all).process!
+    @categories = Api::V1::CategoriesFilterService.new(params, Category.all).process
     render :index
   end
 
