8cf1ffd9c59ca0f1bd943c72298f87b33fb1c1c3
admin <admin@admins-MacBook-Pro.local>
2025-04-13 16:42:58 +0530

    test corrected

--- test/services/api/v1/categories_filter_service_test.rb
+++ test/services/api/v1/categories_filter_service_test.rb
@@ -11,29 +11,29 @@ class CategoriesFilterServiceTest < ActiveSupport::TestCase
   end
 
   def test_should_return_all_categories_when_search_is_nil
-    filtered_categories = Api::V1::CategoriesFilterService.new({}, @all_categories).process!
+    filtered_categories = Api::V1::CategoriesFilterService.new({}, @all_categories).process
     assert_equal 3, filtered_categories.count
   end
 
   def test_should_return_all_categories_when_search_is_blank
-    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "" }, @all_categories).process!
+    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "" }, @all_categories).process
     assert_equal 3, filtered_categories.count
   end
 
   def test_should_return_matching_categories_when_search_term_matches
-    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "Ruby" }, @all_categories).process!
+    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "Ruby" }, @all_categories).process
     assert_equal 1, filtered_categories.count
     assert_equal "Ruby", filtered_categories.first.name
   end
 
   def test_should_return_matching_categories_case_insensitive
-    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "rails" }, @all_categories).process!
+    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "rails" }, @all_categories).process
     assert_equal 1, filtered_categories.count
     assert_equal "Rails", filtered_categories.first.name
   end
 
   def test_should_return_empty_when_search_term_does_not_match_any_category
-    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "Python" }, @all_categories).process!
+    filtered_categories = Api::V1::CategoriesFilterService.new({ search: "Python" }, @all_categories).process
     assert_empty filtered_categories
   end
 end
